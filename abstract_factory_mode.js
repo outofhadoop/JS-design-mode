27d8e0b380c3d1dab4bb75001219a07bf237f759
outofhadoop <tianyu.han@aliyun.com>
2018-06-25 00:02:36 +0800

    no finish ... 20%

---
+++ abstract_factory_mode.js
@@ -0,0 +1,8 @@
+/**
+ * @name abstract-factory-mode
+ * @todo test and summarize abstract factory mode in pratice
+ * @description 抽象工厂模式：通过对累的工厂抽象使其业务用于对产品类簇的创建，而不负责创建某一产品的实例。
+ * Abstract Factory: The factory's abstraction of the class allows its business to be used to create a cluster of product classes without being responsible for creating instances of a product.
+ * 
+ */
+
