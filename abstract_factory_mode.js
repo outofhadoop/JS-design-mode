46774a359ade1e185543e379e1deacbd18e52166
outofhadoop <tianyu.han@aliyun.com>
2018-07-07 13:42:46 +0800

    abstract factory mode

--- abstract_factory_mode.js
+++ abstract_factory_mode.js
@@ -6,3 +6,30 @@
  * 
  */
 
+
+var VehicleFactory = function ( childClass, superClass ) {
+    // 这里校验是否有这个抽象类
+    if ( typeof VehicleFactory[ superClass ] == 'function' ) {
+        function F() {};
+        F.prototype = new VehicleFactory[ superClass ]();
+        childClass.constructor = childClass;
+        childClass.prototype = new F();
+    } else {
+        return new Error( '没有声明此抽象类!' );
+    }
+}
+
+/**
+ * 这里声明一个抽象类
+ */
+VehicleFactory.AbstractClass = function () {};
+VehicleFactory.AbstractClass.doSomething = function () { 
+    return new Error( 'AbstractClass抽象类的抽象方法未被重写，无法调用!' );
+ }
+
+ /**
+  * 声明子类用抽象方法继承抽象类
+  */
+ var ChildClass = function () {};
+ VehicleFactory( ChildClass, 'AbstractClass' );
+//  到此ChildClass就继承了抽象类AbstractClass的抽象方法,但是并没有重写. 
